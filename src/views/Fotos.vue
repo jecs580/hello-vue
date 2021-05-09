d786cdee05ad0f0931becd51be00716386322a41
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-05-08 20:50:20 -0400

    Uso de rutas con parametros

---
+++ src/views/Fotos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Galeria de foto</h1>
+        <Foto/>
+    </div>
+</template>
+
+<script>
+import Foto from '../components/Fotografia'
+export default {
+    components:{
+        Foto
+    }
+}
+</script>
