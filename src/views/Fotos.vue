f40cc7c47f1c2c699867ecf705ee2d43d7e23fd9
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-05-09 23:18:59 -0400

    Uso de rutas dinamicas desde html y javascript

--- src/views/Fotos.vue
+++ src/views/Fotos.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
         <h1>Galeria de foto</h1>
+
+        <router-link :to="{name:'fotos', params:{id}}" v-for="(id,i) of fotosID" :key="i">
+            <button>foto {{id}}</button>
+        </router-link>
         <Foto/>
+        <button @click="home">Home</button>
+        <button @click="anterior">Anterior</button>
+        <button @click="siguiente">Siguiente</button>
     </div>
 </template>
 
@@ -10,6 +17,22 @@ import Foto from '../components/Fotografia'
 export default {
     components:{
         Foto
+    },
+    data(){
+        return{
+            fotosID:[1,2,3]
+        }
+    },
+    methods:{
+        home(){
+            this.$router.push('/')
+        },
+        anterior(){
+            this.$router.go(-1);
+        },
+        siguiente(){
+            this.$router.go(1);
+        }
     }
 }
 </script>
